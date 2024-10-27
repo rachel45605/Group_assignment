29631b038c20683e3a9fc7effdc6b22b235ec552
rachel45605 <rachel45605@gmail.com>
2024-10-27 14:34:12 +0800

    change 4

--- app.py
+++ app.py
@@ -176,6 +176,10 @@ def logout():
     session.pop('user_id', None)
     return redirect(url_for('index'))
 
+@app.route('/advice')
+def advice():
+    return render_template('advice.html')
+
 # if __name__ == '__main__':
 #     with app.app_context():
 #         db.create_all()
