9006e2bb5ded90ca4651c30d3b144c114368cf84
rachel45605 <rachel45605@gmail.com>
2024-10-27 15:36:05 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,13 @@ from werkzeug.security import generate_password_hash, check_password_hash
 import google.generativeai as genai
 import os
 from datetime import datetime
+import os
+
 
 
 app = Flask(__name__)
 # app.config['SECRET_KEY'] = 'your-secret-key-here'
+app.secret_key = os.urandom(24)  # Generates a random secret key
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bank.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # Configure the AI model
